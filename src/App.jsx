8241c4b96dcd500a726ba007597fea21b964484e
Noah Fajarda <noahfajarda@gmail.com>
2023-10-16 10:40:36 -0700

    reset

--- src/App.jsx
+++ src/App.jsx
@@ -28,10 +28,8 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more test
+        Click on the Vite and React logos to learn more
       </p>
-      <FirstBranch />
-      <SecondBranch />
     </>
   );
 }
--- src/components/FirstBranch.jsx
+++
@@ -1,7 +0,0 @@
-export default function FirstBranch() {
-  return (
-    <div>
-      <div style={{ color: "red" }}>firstBranch component right here</div>
-    </div>
-  );
-}
