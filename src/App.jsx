c3250b31d7ed5ef569e5c4343cf42fbb2271cee2
Noah Fajarda <noahfajarda@gmail.com>
2023-10-16 11:10:15 -0700

    initial commit second branch

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import FirstBranch from "./components/firstBranch";
 import SecondBranch from "./components/secondBranch";
 
 function App() {
@@ -30,6 +29,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <SecondBranch />
     </>
   );
 }
