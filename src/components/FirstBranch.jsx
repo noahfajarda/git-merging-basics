195188536da408e9de4752d350311c5d3eb5d659
Noah Fajarda <noahfajarda@gmail.com>
2023-10-16 10:07:58 -0700

    added tailwind installation docs

---
+++ src/components/FirstBranch.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function FirstBranch() {
+  return (
+    <div>
+      <div style={{ color: "red" }}>firstBranch component right here</div>
+    </div>
+  );
+}
